5a2119efc6d652184d1b10b2d0c62dff9e9b7d74
Pietro Degrazia <pietrodegrazia@hotmail.com>
2018-05-03 18:58:56 -0300

    now printing ast to std out.

--- etapa3/ast.c
+++ etapa3/ast.c
@@ -20,64 +20,238 @@ AST* astCreate(int type, symbol_t* symbol, struct AST_s *firstChild, struct AST_
 void astPrint(AST *node, int level) {
 	int i;
 	if (!node){ 
-		// fprintf(stderr, "no node. returning.\n");
 		return;
 	}
 	for(i = 0; i < level; ++i) {
-		fprintf(stderr, "   ");
+		printf("   ");
 	}
-	fprintf(stderr, "AST(");
-	switch(node->type)  {
+	printf("AST(");
+	switch(node->type) {
+		
+		case AST_PROGRAM:
+			printf("AST_PROGRAM)\n");
+			astPrint(node->children[0],level + 1);//declaration list
+			break;
+
+		case AST_DEC_LIST:
+			printf("AST_DEC_LIST)\n");
+			astPrint(node->children[0],level + 1);//declaration of value, vector, initialized vector or function
+			astPrint(node->children[1],level + 1);//another declaration list
+			break;
+
+		case AST_DEC_VALUE:
+			printf("AST_DEC_VALUE)\n");
+			astPrint(node->children[0], level + 1);
+			break;
+
 		case AST_DEC_VECTOR:
-			fprintf(stderr, "AST_DEC_VECTOR, %s)\n",node->symbol->key);
+			printf("AST_DEC_VECTOR)\n");
+			astPrint(node->children[0], level + 1);
 			break;
-		case AST_PARAM_LIST:
-			fprintf(stderr, "AST_PARAM_LIST)\n");
+
+		case AST_DEC_VECTOR_INIT:
+			printf("AST_DEC_VECTOR_INIT)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_PARAM:
-			fprintf(stderr, "AST_PARAM,%s)\n",node->symbol->key);
+
+		case AST_DEC_POINTER:
+			printf("AST_DEC_POINTER)\n");
+			astPrint(node->children[0], level + 1);
 			break;
+
 		case AST_DEC_FUNC:
-			fprintf(stderr, "AST_DEC_FUNC,%s)\n",node->symbol->key);
+			printf("AST_DEC_FUNC, %s)\n", node->symbol->key);
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_VALUE_LIST: 
-			fprintf(stderr, "AST_VALUE_LIST)\n");
+
+		case AST_PARAM_LIST:
+			printf("AST_PARAM_LIST)\n");
+			astPrint(node->children[0], level + 1);
+			if(node->children[1]) {
+				astPrint(node->children[1], level + 1);
+			}
 			break;
-		case AST_SYMBOL: 
-			fprintf(stderr, "AST_SYMBOL,%s)\n",node->symbol->key);
+
+		case AST_PARAM:
+			printf("AST_PARAM, %s)\n", node->symbol->key);
 			break;
+
+		case AST_VALUE_LIST:
+			printf("AST_VALUE_LIST)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
+		case AST_SYMBOL:
+			printf("AST_SYMBOL, %s)\n", node->symbol->key);
+			break;
+
+		case AST_SYMBOL_ADDRESS:
+			printf("AST_SYMBOL_ADDRESS, %s)\n", node->symbol->key);
+			break;
+
+		case AST_SYMBOL_POINTER:
+			printf("AST_SYMBOL_POINTER, %s)\n", node->symbol->key);
+			break;
+
+		case AST_BLOCK:
+			printf("AST_BLOCK)\n");
+			astPrint(node->children[0], level + 1);
+			break;
+
+		case AST_CMD_LIST:
+			printf("AST_CMD_LIST)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
+		case AST_VALUE_ASS:
+			printf("AST_VALUE_ASS, %s)\n", node->symbol->key);
+			astPrint(node->children[0], level + 1);
+			break;
+
+		case AST_VECTOR_ASS:
+			printf("AST_VECTOR_ASS, %s)\n", node->symbol->key);
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
+		case AST_VECTOR_ACCESS:
+			printf("AST_VECTOR_ACCESS, %s)\n", node->symbol->key);
+			astPrint(node->children[0], level + 1);
+			break;
+
+		case AST_READ:
+			printf("AST_READ, %s)\n", node->symbol->key);
+			astPrint(node->children[0], level + 1);
+			break;
+
+		case AST_PRINT:
+			printf("AST_PRINT)\n");
+			astPrint(node->children[0], level + 1);
+			break;
+
+		case AST_ELEM_LIST:
+			printf("AST_ELEM_LIST)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
+		case AST_RETURN:
+			printf("AST_RETURN)\n");
+			astPrint(node->children[0], level + 1);
+			break;
+
+		case AST_IF_THEN:
+			printf("AST_IF_THEN)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
+		case AST_IF_THEN_ELSE:
+			printf("AST_IF_THEN_ELSE)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			astPrint(node->children[2], level + 1);
+			break;
+
+		case AST_WHILE:
+			printf("AST_WHILE)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
+		case AST_FOR:
+			printf("AST_FOR)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			astPrint(node->children[2], level + 1);
+			break;
+
+		case AST_INVOKE_FUNC:
+			printf("AST_INVOKE_FUNC, %s)\n", node->symbol->key);
+			astPrint(node->children[0], level + 1);
+			break;
+			
+		case AST_ARG_LIST:
+			printf("AST_ARG_LIST)\n");
+			astPrint(node->children[0], level + 1);
+			if(node->children[1]) {
+				astPrint(node->children[1], level + 1);
+			}
+			break;
+
+		case AST_NOT:
+			printf("AST_NOT)\n");
+			astPrint(node->children[0], level + 1);
+			break;
+
 		case AST_ADD:
-			fprintf(stderr, "AST_ADD)\n");
+			printf("AST_ADD)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
 		case AST_SUB:
-			fprintf(stderr, "AST_SUB)\n");
+			printf("AST_SUB)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_CMD_LIST:
-			fprintf(stderr, "AST_CMD_LIST)\n");
+		case AST_MULT:
+			printf("AST_MULT)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_DEC_VALUE:
-			fprintf(stderr, "AST_DEC_VALUE,%s)\n",node->symbol->key);
+		case AST_DIV:
+			printf("AST_DIV)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_PROGRAM:
-			fprintf(stderr, "AST_PROGRAM)\n");
+		case AST_LESS:
+			printf("AST_LESS)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_BLOCK:
-			fprintf(stderr, "AST_BLOCK)\n");
+		case AST_GREATER:
+			printf("AST_GREATER)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_FOR:
-			fprintf(stderr, "AST_FOR)\n");
+		case AST_LESS_EQ:
+			printf("AST_LESS_EQ)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_PRINT:
-			fprintf(stderr, "AST_PRINT)\n");
+		case AST_GREATER_EQ:
+			printf("AST_GREATER_EQ)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
-		case AST_ELEM_LIST:
-			fprintf(stderr, "AST_ELEM_LIST)\n");
+		case AST_EQ:
+			printf("AST_EQ)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
 			break;
+		case AST_NEQ:
+			printf("AST_NEQ)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+		case AST_AND:
+			printf("AST_AND)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+		case AST_OR:
+			printf("AST_OR)\n");
+			astPrint(node->children[0], level + 1);
+			astPrint(node->children[1], level + 1);
+			break;
+
 		default:
-			fprintf(stderr, "UNRECOGNIZED AST TYPE)\n");
-			break;	
+			printf("Not implemented yet. ast-> %d\n",node->type);
+			break;
 	}
-	for(i=0; i < MAX_CHILDREN; ++i) {
-		astPrint(node->children[i], level + 1);
-	}
+	return;
 }
